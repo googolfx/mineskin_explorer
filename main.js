12f10af2d92634ddef18732dbfde56d4c29f2133
WhiteBell <googolfx@gmail.com>
2023-09-28 11:01:15 +0900

    What the mistakes...

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Hey Electron?');
+//console.log('Hey Electron?');
+
+const { app, BrowserWindow } = require('electron');
+
+app.on('ready', function() {
+
+    win = new BrowserWindow({ width: 1280, height: 720 });
+
+    win.loadFile('index.html');
+})
